3f272d0cb3b5d17d87e4a4c1e36bb470b1225e3c
q <commitment1511@outlook.com>
2022-08-26 18:32:29 +0700

    ...

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ import bodyParser from "body-parser";
 import initAppRoutes from "./routes/index";
 const { configCors } = require("./config/cors");
 // const { connection } = require('./config/connectDB')
-
 require("dotenv").config();
 
 const app = express();
+
+const cookieParser = require("cookie-parser");
 
 const hostname = process.env.HOST_NAME;
 const PORT = process.env.PORT || 8080;
@@ -16,6 +17,7 @@ configCors(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // connection()
 
