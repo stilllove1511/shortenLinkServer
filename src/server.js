cc0b0c5408992a1d02db6d0c69dc7f1171b7cfa0
q <commitment1511@outlook.com>
2022-09-01 20:47:55 +0700

    cors npm

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser"
-
 import initAppRoutes from "./routes/index"
+const cors = require("cors")
 const { configCors } = require("./config/cors")
 const { connection } = require("./config/connectDB")
 require("dotenv").config()
@@ -13,12 +13,18 @@ const cookieParser = require("cookie-parser")
 // const hostname = process.env.HOST_NAME
 const PORT = process.env.PORT || 8080
 
-configCors(app)
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-
+app.use(
+    cors({
+        origin: ["http://localhost:3000", process.env.REACT_URL],
+        credentials: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        preflightContinue: false,
+        optionsSuccessStatus: 200,
+    })
+)
 // connection()
 
 initAppRoutes(app)
