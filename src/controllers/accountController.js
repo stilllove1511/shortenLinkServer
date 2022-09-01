596b154d452d18415525ab2b0c8c068e66daa124
q <commitment1511@outlook.com>
2022-09-01 21:58:46 +0700

    log origin

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,34 +1,35 @@
-import accountService from "../services/accountService";
+import accountService from "../services/accountService"
 
 const updatePassword = async (req, res) => {
     try {
-        console.log(req.user);
+        console.log(req.user)
         let data = await accountService.updatePassword({
             ...req.body,
             userId: req.user.id,
-        });
+        })
 
         return res.status(200).json({
             EM: data.EM,
             EC: data.EC,
             DT: "",
-        });
+        })
     } catch (error) {
-        console.log(error);
+        console.log(error)
         return res.status(500).json({
             EM: "error fom server",
             EC: "-1",
             DT: "",
-        });
+        })
     }
-};
+}
 
 const getUserInfor = (req, res) => {
+    console.log("origin:", req.headers.origin)
     res.status(200).json({
         EM: "get user infor succesfully!!",
         EC: 0,
         DT: req.user,
-    });
-};
+    })
+}
 
-export default { updatePassword, getUserInfor };
+export default { updatePassword, getUserInfor }
