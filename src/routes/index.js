09c8c24d1f48e3407c1d7cf5614e8ff1f8f13799
q <commitment1511@outlook.com>
2022-09-03 13:28:54 +0700

    ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
-import logRouter from "./log";
-import linkRouter from "./link";
-import shortenLinkRouter from "./shortenLink";
-import accountRouter from "./account";
+import logRouter from "./log"
+import linkRouter from "./link"
+import accountRouter from "./account"
 
 export default (app) => {
-    app.use(shortenLinkRouter);
-    app.use(logRouter);
-    app.use("/link", linkRouter);
-    app.use("/account", accountRouter);
-};
+    app.use(logRouter)
+    app.use("/link", linkRouter)
+    app.use("/account", accountRouter)
+}
