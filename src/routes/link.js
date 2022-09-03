0ed7ccb961d38ba903bbe590895b39cd67f25345
q <commitment1511@outlook.com>
2022-09-03 21:15:45 +0700

    link/getall route remove

--- src/routes/link.js
+++ src/routes/link.js
@@ -5,7 +5,6 @@ import linkController from "../controllers/linkController"
 const linkRouter = express.Router()
 
 linkRouter.get("/read", checkUserJWT, linkController.readLink)
-linkRouter.get("/readAll", linkController.readAllLink)
 linkRouter.post("/create", checkUserJWT, linkController.createLink)
 linkRouter.put("/update", checkUserJWT, linkController.updateLink)
 linkRouter.delete("/delete", checkUserJWT, linkController.deleteLink)
