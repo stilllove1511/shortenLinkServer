56df471158989f5b9e1d95edaf8f2a1e38ba7bf8
Your Name <commitment1511@outlook.com>
2023-04-08 16:24:26 +0700

    test piple line

--- src/server.js
+++ src/server.js
@@ -1,43 +1,43 @@
 import express from "express"
-// import bodyParser from "body-parser"
-// import initAppRoutes from "./routes/index"
-// import { connectRedis } from "./redis"
-// import { connectMongo } from "./mongo"
-// import { configCors } from "./config/cors"
-// import cookieParser from "cookie-parser"
-// import session from "express-session"
-// const passport = require("passport")
+import bodyParser from "body-parser"
+import initAppRoutes from "./routes/index"
+import { connectRedis } from "./redis"
+import { connectMongo } from "./mongo"
+import { configCors } from "./config/cors"
+import cookieParser from "cookie-parser"
+import session from "express-session"
+const passport = require("passport")
 
-// require("dotenv").config()
+require("dotenv").config()
 const app = express()
-// const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(cookieParser())
-// app.use(
-//     session({
-//         secret: "keyboard cat",
-//         resave: false,
-//         saveUninitialized: false,
-//     })
-// )
-// app.use(passport.authenticate("session"))
-// passport.serializeUser(function (user, cb) {
-//     process.nextTick(function () {
-//         cb(null, user)
-//     })
-// })
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+app.use(passport.authenticate("session"))
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, user)
+    })
+})
 
-// passport.deserializeUser(function (user, cb) {
-//     process.nextTick(function () {
-//         return cb(null, user)
-//     })
-// })
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user)
+    })
+})
 
-// configCors(app)
+configCors(app)
 
-// initAppRoutes(app)
+initAppRoutes(app)
 
 // connect mongo
 // connectMongo()
